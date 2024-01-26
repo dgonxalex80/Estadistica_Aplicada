fa1631f5323ccf27319adf14e69f113fea84d430
Daniel E. Gonzalez <dgonxalex80@gmail.com>
2024-01-26 09:55:28 -0500

    ajustes taller001

--- R/Taller001.R
+++ R/Taller001.R
@@ -13,9 +13,15 @@
 # TeachingSampling (conjunto de datos)
 # SamplingBook (calculo de tamaños de muestras y muestreo)
 # samplingUtil (calculo de tamaños de muestras)
-# suervey (Inferencias para muestras complejas) Todas las librerías se encuentran en el repositoria oficial de R excepto la librería SamplingUtil, necesitará installar la librería devTools para posteriormente utilizar el comando install_github() para poder obtenerla.
+# suervey (Inferencias para muestras complejas) 
 
-# 1. Instalación y carga de las librerias -------------
+# Todas las librerías se encuentran en el repositoria oficial de R 
+# excepto la librería SamplingUtil, necesitará installar 
+# la librería devTools para posteriormente utilizar el 
+# comando install_github() para poder obtenerla.
+
+
+# 1. Instalación y carga de las librerias ---------------
 # install.packages("TeachingSampling")
 # install.packages("tidyverse")
 # install.packages("dslabs")
@@ -28,80 +34,66 @@ library(dslabs)
 
 data(BigLucy)   # carga de datos BigLucy
 str(BigLucy)  # estructura de la data
-
-# Nota : Supondremos que estos datos corresponden a una población
+# Nota : Supondremos que estos datos 
+#        corresponden a una población
+#        Explora la data - indicadores / graficas
 
 
 # 3. Cálculo de algunos de los parámetros de interés
+
 mean(BigLucy$Income)   # media de Income - ingresos
 prop.table(table(BigLucy$SPAM)) # proporción de SPAM
 
 
-# MUESTREO ALEATORIO SIMPLE
-library(TeachingSampling)
-data(BigLucy)    # Cargamos el conjunto de datos
-set.seed(123) # Definimos el valor de la semilla para obterner resultos reproducibles.
-tamaño = 10
+# MUESTREO ALEATORIOS
 
-Lucy  %>% 
-  slice_sample(n = tamaño) -> muestra
-muestra
-# Calculo de estimadores
-mean(muestra$Income)
-prop.table(table(muestra$SPAM))
-
-# -------------------------------------
-
-# Cargar el paquete dslabs 
-install.packages("dslabs")
+## Muestreo aleatorio simple -------------------------
+# install.packages("dslabs")
 library(dslabs)
-
-
-# Mostrar la estructura de la data
-str(BigLucy)
-
-# Ejemplos de muestreo con el conjunto de datos (adapta según tus datos)
-set.seed(123)
+set.seed(123)  # fijar semilla
 #-----------------------------------------------------
-# Muestreo Probabilístico Simple
-n= 20
-muestra_simple <- BigLucy[sample(nrow(BigLucy), n), ]
-print(muestra_simple)
-# -----------------------------------------------------
-# Muestreo Estratificado
+# 4. Muestreo Probabilístico Simple
+n= 20  # tamaño de la muestra 
+muestra_simple <- BigLucy[sample(nrow(BigLucy), n), ] 
+muestra_simple
+
+
+# 5. Muestreo Estratificado --------------------------
 library(dplyr)
 muestra_estratificada <- BigLucy %>% 
   group_by(ISO) %>% 
   sample_n(10)
-print(muestra_estratificada)
+muestra_estratificada
 
-#-----------------------------------------------------
-# Muestreo por Conglomerados
+
+# 6. Muestreo por Conglomerados ----------------------
 
 # Elegir un conglomerado al azar
 conglomerado_elegido <- sample(unique(BigLucy$Zone), 1)
 
-# Extraer una muestra de tamaño n=10 del conglomerado seleccionado
+# Extraer una muestra de tamaño n=20 del conglomerado 
+# seleccionado
 muestra_conglomerado_especifico <- BigLucy %>% 
   filter(Zone == conglomerado_elegido) %>%
-  sample_n(20, replace = TRUE)  # Puedes ajustar replace según tus necesidades
+  sample_n(20, replace = TRUE)  
+muestra_conglomerado_especifico
 
-# Imprimir la muestra del conglomerado seleccionado
-print(muestra_conglomerado_especifico)
 #---------------------------------------------------
-# Muestreo No 
-# Muestreo por conveniencia
-# muestra de los primeros n registros
-muestra_conveniencia <- BigLucy[1:20, ]
-print(muestra_conveniencia)
+# MUESTREOS NO PROBABILISATICOS 
 
-# -------------------------------------------------
-# Muestreo por juicio
-muestra_juicio <- BigLucy[c(1, 5, 10, 15, 20), ]
-print(muestra_juicio)m
+# Muestreo por conveniencia ------------------------
+muestra_conveniencia <- BigLucy[1:20, ] # 20  primeras del listado
+muestra_conveniencia
 
-# Muestreo Bola de Nieve 
+
+# Muestreo por juicio -----------------------------
+muestra_juicio <- BigLucy[c(1, 5, 10, 15, 20), ] # seleccionadas a ojo
+muestra_juicio
+
+# Muestreo Bola de Nieve  ------------------------
+# se requiere información previa
 
 #---------------------------------------------
-write.csv(BigLucy, file = "data/BigLucy.csv", row.names = FALSE)
-BigLucy1 <- read.csv("data/BigLucy.csv")
+write.csv(BigLucy, file = "data/BigLucy.csv", row.names = FALSE) # guarda la base en formato .csv
+BigLucy1 <- read.csv("data/BigLucy.csv") # lee la data de la misma carpeta
+#------------------------------------------------
